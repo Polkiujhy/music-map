ed8164bc4bf7de706d6c34ab8c3147529995b445
Polkiujhy <adam.kurek012@gmail.com>
2026-09-11 19:05:05 +0100

    Trust forwarded HTTPS scheme

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,23 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_asset_urls_use_forwarded_https_scheme(): void
+    {
+        $response = $this
+            ->withServerVariables([
+                'HTTP_HOST' => 'music.adamis.me',
+                'REMOTE_ADDR' => '172.23.0.8',
+            ])
+            ->withHeaders([
+                'X-Forwarded-Host' => 'attacker.invalid',
+                'X-Forwarded-Proto' => 'https',
+            ])
+            ->get('/');
+
+        $response->assertOk();
+        $response->assertSee('https://localhost:8000/build/', false);
+        $response->assertDontSee('http://localhost:8000/build/', false);
+        $response->assertDontSee('attacker.invalid', false);
+    }
 }
